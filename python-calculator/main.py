f3cc530a72311da547437421347ff0b185167f24
ravindu0823 <guestpc87@gmail.com>
2023-10-15 14:32:32 +0530

    feat: create file for python calculator and created functions for add, sub, mul, div

---
+++ python-calculator/main.py
@@ -0,0 +1,13 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+    return x / y
