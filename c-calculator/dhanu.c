4bc21254097b61bc2b936407a0d3399a8fac09fa
ravindu0823 <guestpc87@gmail.com>
2023-10-15 15:24:21 +0530

    feat: create function for power and squreRoom

--- c-calculator/dhanu.c
+++ c-calculator/dhanu.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
 #include <math.h>
 
-// Function to perform addition
 double add(double a, double b) {
     return a + b;
 }
 
-// Function to perform subtraction
 double subtract(double a, double b) {
     return a - b;
 }
 
-// Function to perform multiplication
 double multiply(double a, double b) {
     return a * b;
 }
 
-// Function to perform division
 double divide(double a, double b) {
     if (b == 0) {
         printf("Error: Division by zero is not allowed.\n");
         return 0.0;
     }
     return a / b;
+}
+
+// Function to perform exponentiation
+double power(double base, double exponent) {
+    return pow(base, exponent);
+}
+
+// Function to calculate the square root
+double squareRoot(double num) {
+    if (num < 0) {
+        printf("Error: Cannot calculate square root of a negative number.\n");
+        return 0.0;
+    }
+    return sqrt(num);
 }
 
 int main() {
