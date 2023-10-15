ff71e2224e030f8aacbc7af3ac44459130d6ec72
ravindu0823 <guestpc87@gmail.com>
2023-10-15 14:41:14 +0530

    chore: get the operator value

--- c-calculator/main.c
+++ c-calculator/main.c
@@ -6,6 +6,16 @@ int main() {
 
     printf("Enter an operator (+, -, *, /): ");
     scanf(" %c", &operator);
+
+    if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+        printf("Invalid operator entered.");
+        return 1; // Exit with an error code
+    }
+
+    printf("Enter number one: ");
+    scanf("%lf", &num1);
+    printf("Enter number two: ");
+    scanf("%lf", &num2);
     
     return 0; // Exit with a success code
 }
