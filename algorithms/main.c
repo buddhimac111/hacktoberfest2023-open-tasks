0dfbc028deb86eccba621a0530914577e4596e83
ravindu0823 <guestpc87@gmail.com>
2023-10-15 15:10:26 +0530

    chore: create optimized bubble sort algorithm

--- algorithms/main.c
+++ algorithms/main.c
@@ -7,6 +7,24 @@ int main()
     int i, j, temp, size = 5;
     int pass = 0;
     int ex = 1;
+
+    while (i < size && ex != 0)
+    {
+        ex = 0;
+
+        for (j = 0; j < (size - 1); j++)
+        {
+            if (data[j] > data[j + 1])
+            {
+                temp = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = temp;
+                ex = ex + 1;
+            }
+        }
+        pass = pass + 1;
+        i = i + 1;
+    }
     
 
     return 0;
