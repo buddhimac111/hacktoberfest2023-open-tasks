dda826806cb0c264e64f4923a483367ff92b6bb4
ravindu0823 <guestpc87@gmail.com>
2023-10-15 14:41:46 +0530

    chore: check the operator and perform task

--- c-calculator/main.c
+++ c-calculator/main.c
@@ -16,6 +16,25 @@ int main() {
     scanf("%lf", &num1);
     printf("Enter number two: ");
     scanf("%lf", &num2);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 == 0) {
+                printf("Error: Division by zero is not allowed.\n");
+                return 1; // Exit with an error code
+            }
+            result = num1 / num2;
+            break;
+    }
     
     return 0; // Exit with a success code
 }
