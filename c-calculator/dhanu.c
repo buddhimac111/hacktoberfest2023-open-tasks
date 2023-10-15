66adf2386b10ab2d5cef6e5738a44171d90ce539
ravindu0823 <guestpc87@gmail.com>
2023-10-15 15:23:42 +0530

    feat: create function sub, mul, div

--- c-calculator/dhanu.c
+++ c-calculator/dhanu.c
@@ -6,6 +6,25 @@ double add(double a, double b) {
     return a + b;
 }
 
+// Function to perform subtraction
+double subtract(double a, double b) {
+    return a - b;
+}
+
+// Function to perform multiplication
+double multiply(double a, double b) {
+    return a * b;
+}
+
+// Function to perform division
+double divide(double a, double b) {
+    if (b == 0) {
+        printf("Error: Division by zero is not allowed.\n");
+        return 0.0;
+    }
+    return a / b;
+}
+
 int main() {
 
     return 0;
