42feaa35a3190ffe4dc02d41779863fb5a7cd358
subo <87760394+hettiarachchisp526@users.noreply.github.com>
2023-10-17 23:23:29 +0530

    Add files via upload

---
+++ open-scripts/my_portfolio_web/script.js
@@ -0,0 +1,19 @@
+/*nav bar showing*/
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 50);
+});
+
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick =() => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
