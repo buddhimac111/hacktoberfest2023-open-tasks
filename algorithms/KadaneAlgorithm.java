9ba6e8736feda23a9af4b172bc2435186840aef1
alpesh677 <bariaalpesh477@gmail.com>
2023-10-19 23:37:06 +0530

    added kadane's algorithm in algorithm folder

---
+++ algorithms/KadaneAlgorithm.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class KadaneAlgorithm {
+    public static int maxSubarraySum(int[] nums) {
+        int maxEndingHere = nums[0];
+        int maxSoFar = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+
+        return maxSoFar;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int maxSum = maxSubarraySum(arr);
+
+        System.out.println("Maximum Subarray Sum: " + maxSum);
+
+        scanner.close();
+    }
+}
